5868412d12fe63d89d522bead01d712e2426a90e
Adam Serbinski <adam@serbinski.com>
2018-07-26 16:34:36 -0400

    I2S Mezzanine: Create a board

---
+++ boards/gEDA/i2s-mezzanine/i2s_mezzanine.cmd
@@ -0,0 +1,583 @@
+# Pin name action command file
+
+# Start of element CONN501
+ChangePinName(CONN501, 5, 5)
+ChangePinName(CONN501, 6, 6)
+ChangePinName(CONN501, 3, 3)
+ChangePinName(CONN501, 4, 4)
+ChangePinName(CONN501, 2, 2)
+ChangePinName(CONN501, 1, 1)
+
+# Start of element C507
+ChangePinName(C507, 1, 1)
+ChangePinName(C507, 2, 2)
+
+# Start of element C506
+ChangePinName(C506, 1, 1)
+ChangePinName(C506, 2, 2)
+
+# Start of element C505
+ChangePinName(C505, 1, 1)
+ChangePinName(C505, 2, 2)
+
+# Start of element C504
+ChangePinName(C504, 1, 1)
+ChangePinName(C504, 2, 2)
+
+# Start of element R505
+ChangePinName(R505, 1, 1)
+ChangePinName(R505, 2, 2)
+
+# Start of element C503
+ChangePinName(C503, 1, 1)
+ChangePinName(C503, 2, 2)
+
+# Start of element R502
+ChangePinName(R502, 1, 1)
+ChangePinName(R502, 2, 2)
+
+# Start of element R501
+ChangePinName(R501, 1, 1)
+ChangePinName(R501, 2, 2)
+
+# Start of element R504
+ChangePinName(R504, 1, 1)
+ChangePinName(R504, 2, 2)
+
+# Start of element R503
+ChangePinName(R503, 1, 1)
+ChangePinName(R503, 2, 2)
+
+# Start of element C502
+ChangePinName(C502, 1, 1)
+ChangePinName(C502, 2, 2)
+
+# Start of element C501
+ChangePinName(C501, 1, 1)
+ChangePinName(C501, 2, 2)
+
+# Start of element U501
+ChangePinName(U501, 5, VO2)
+ChangePinName(U501, 4, IN2-)
+ChangePinName(U501, 3, /EN)
+ChangePinName(U501, 2, GND)
+ChangePinName(U501, 6, VDD)
+ChangePinName(U501, 7, VO1)
+ChangePinName(U501, 1, BYP)
+ChangePinName(U501, 8, IN1-)
+
+# Start of element CONN406
+ChangePinName(CONN406, 4, 4)
+ChangePinName(CONN406, 3, 3)
+ChangePinName(CONN406, 1, 1)
+ChangePinName(CONN406, 2, 2)
+
+# Start of element CONN405
+ChangePinName(CONN405, 4, 4)
+ChangePinName(CONN405, 3, 3)
+ChangePinName(CONN405, 1, 1)
+ChangePinName(CONN405, 2, 2)
+
+# Start of element CONN404
+ChangePinName(CONN404, 4, 4)
+ChangePinName(CONN404, 3, 3)
+ChangePinName(CONN404, 1, 1)
+ChangePinName(CONN404, 2, 2)
+
+# Start of element CONN403
+ChangePinName(CONN403, 4, 4)
+ChangePinName(CONN403, 3, 3)
+ChangePinName(CONN403, 1, 1)
+ChangePinName(CONN403, 2, 2)
+
+# Start of element CONN402
+ChangePinName(CONN402, 4, 4)
+ChangePinName(CONN402, 3, 3)
+ChangePinName(CONN402, 1, 1)
+ChangePinName(CONN402, 2, 2)
+
+# Start of element CONN401
+ChangePinName(CONN401, 4, 4)
+ChangePinName(CONN401, 3, 3)
+ChangePinName(CONN401, 1, 1)
+ChangePinName(CONN401, 2, 2)
+
+# Start of element C410
+ChangePinName(C410, 1, 1)
+ChangePinName(C410, 2, 2)
+
+# Start of element R404
+ChangePinName(R404, 1, 1)
+ChangePinName(R404, 2, 2)
+
+# Start of element C409
+ChangePinName(C409, 1, 1)
+ChangePinName(C409, 2, 2)
+
+# Start of element R403
+ChangePinName(R403, 1, 1)
+ChangePinName(R403, 2, 2)
+
+# Start of element C408
+ChangePinName(C408, 1, 1)
+ChangePinName(C408, 2, 2)
+
+# Start of element R402
+ChangePinName(R402, 1, 1)
+ChangePinName(R402, 2, 2)
+
+# Start of element C407
+ChangePinName(C407, 1, 1)
+ChangePinName(C407, 2, 2)
+
+# Start of element R401
+ChangePinName(R401, 1, 1)
+ChangePinName(R401, 2, 2)
+
+# Start of element U304
+ChangePinName(U304, 3, 3)
+ChangePinName(U304, 4, 4)
+ChangePinName(U304, 2, 2)
+ChangePinName(U304, 1, 1)
+
+# Start of element C332
+ChangePinName(C332, 1, 1)
+ChangePinName(C332, 2, 2)
+
+# Start of element C333
+ChangePinName(C333, 1, 1)
+ChangePinName(C333, 2, 2)
+
+# Start of element C327
+ChangePinName(C327, 1, 1)
+ChangePinName(C327, 2, 2)
+
+# Start of element C324
+ChangePinName(C324, 1, 1)
+ChangePinName(C324, 2, 2)
+
+# Start of element C318
+ChangePinName(C318, 1, 1)
+ChangePinName(C318, 2, 2)
+
+# Start of element C331
+ChangePinName(C331, 1, 1)
+ChangePinName(C331, 2, 2)
+
+# Start of element C320
+ChangePinName(C320, 1, 1)
+ChangePinName(C320, 2, 2)
+
+# Start of element C325
+ChangePinName(C325, 1, 1)
+ChangePinName(C325, 2, 2)
+
+# Start of element C319
+ChangePinName(C319, 1, 1)
+ChangePinName(C319, 2, 2)
+
+# Start of element C321
+ChangePinName(C321, 1, 1)
+ChangePinName(C321, 2, 2)
+
+# Start of element C322
+ChangePinName(C322, 1, 1)
+ChangePinName(C322, 2, 2)
+
+# Start of element C323
+ChangePinName(C323, 1, 1)
+ChangePinName(C323, 2, 2)
+
+# Start of element U303
+ChangePinName(U303, 28, DVDD)
+ChangePinName(U303, 27, DGND)
+ChangePinName(U303, 26, LDOO)
+ChangePinName(U303, 25, XSMT)
+ChangePinName(U303, 24, ADR1)
+ChangePinName(U303, 23, LRCK)
+ChangePinName(U303, 22, DIN)
+ChangePinName(U303, 21, BCK)
+ChangePinName(U303, 20, SCK)
+ChangePinName(U303, 19, GPIO6)
+ChangePinName(U303, 18, MODE2)
+ChangePinName(U303, 17, MODE1)
+ChangePinName(U303, 16, ADR2)
+ChangePinName(U303, 15, GPIO3)
+ChangePinName(U303, 14, GPIO4)
+ChangePinName(U303, 13, GPIO5)
+ChangePinName(U303, 12, SCL)
+ChangePinName(U303, 11, SDA)
+ChangePinName(U303, 10, VCOM)
+ChangePinName(U303, 9, AGND)
+ChangePinName(U303, 8, AVDD)
+ChangePinName(U303, 7, OUTR)
+ChangePinName(U303, 6, OUTL)
+ChangePinName(U303, 5, VNEG)
+ChangePinName(U303, 4, CAPM)
+ChangePinName(U303, 3, CPGND)
+ChangePinName(U303, 2, CAPP)
+ChangePinName(U303, 1, CPVDD)
+
+# Start of element C326
+ChangePinName(C326, 1, 1)
+ChangePinName(C326, 2, 2)
+
+# Start of element C328
+ChangePinName(C328, 1, 1)
+ChangePinName(C328, 2, 2)
+
+# Start of element C329
+ChangePinName(C329, 1, 1)
+ChangePinName(C329, 2, 2)
+
+# Start of element C330
+ChangePinName(C330, 1, 1)
+ChangePinName(C330, 2, 2)
+
+# Start of element U302
+ChangePinName(U302, 28, DVDD)
+ChangePinName(U302, 27, DGND)
+ChangePinName(U302, 26, LDOO)
+ChangePinName(U302, 25, XSMT)
+ChangePinName(U302, 24, ADR1)
+ChangePinName(U302, 23, LRCK)
+ChangePinName(U302, 22, DIN)
+ChangePinName(U302, 21, BCK)
+ChangePinName(U302, 20, SCK)
+ChangePinName(U302, 19, GPIO6)
+ChangePinName(U302, 18, MODE2)
+ChangePinName(U302, 17, MODE1)
+ChangePinName(U302, 16, ADR2)
+ChangePinName(U302, 15, GPIO3)
+ChangePinName(U302, 14, GPIO4)
+ChangePinName(U302, 13, GPIO5)
+ChangePinName(U302, 12, SCL)
+ChangePinName(U302, 11, SDA)
+ChangePinName(U302, 10, VCOM)
+ChangePinName(U302, 9, AGND)
+ChangePinName(U302, 8, AVDD)
+ChangePinName(U302, 7, OUTR)
+ChangePinName(U302, 6, OUTL)
+ChangePinName(U302, 5, VNEG)
+ChangePinName(U302, 4, CAPM)
+ChangePinName(U302, 3, CPGND)
+ChangePinName(U302, 2, CAPP)
+ChangePinName(U302, 1, CPVDD)
+
+# Start of element R310
+ChangePinName(R310, 1, 1)
+ChangePinName(R310, 2, 2)
+
+# Start of element R309
+ChangePinName(R309, 1, 1)
+ChangePinName(R309, 2, 2)
+
+# Start of element C317
+ChangePinName(C317, 1, 1)
+ChangePinName(C317, 2, 2)
+
+# Start of element C316
+ChangePinName(C316, 1, 1)
+ChangePinName(C316, 2, 2)
+
+# Start of element R305
+ChangePinName(R305, 1, 1)
+ChangePinName(R305, 2, 2)
+
+# Start of element R306
+ChangePinName(R306, 1, 1)
+ChangePinName(R306, 2, 2)
+
+# Start of element C315
+ChangePinName(C315, 1, 1)
+ChangePinName(C315, 2, 2)
+
+# Start of element C314
+ChangePinName(C314, 1, 1)
+ChangePinName(C314, 2, 2)
+
+# Start of element C313
+ChangePinName(C313, 1, 1)
+ChangePinName(C313, 2, 2)
+
+# Start of element C312
+ChangePinName(C312, 1, 1)
+ChangePinName(C312, 2, 2)
+
+# Start of element R307
+ChangePinName(R307, 1, 1)
+ChangePinName(R307, 2, 2)
+
+# Start of element R308
+ChangePinName(R308, 1, 1)
+ChangePinName(R308, 2, 2)
+
+# Start of element C301
+ChangePinName(C301, 1, 1)
+ChangePinName(C301, 2, 2)
+
+# Start of element C302
+ChangePinName(C302, 1, 1)
+ChangePinName(C302, 2, 2)
+
+# Start of element C304
+ChangePinName(C304, 1, 1)
+ChangePinName(C304, 2, 2)
+
+# Start of element C303
+ChangePinName(C303, 1, 1)
+ChangePinName(C303, 2, 2)
+
+# Start of element C311
+ChangePinName(C311, 1, 1)
+ChangePinName(C311, 2, 2)
+
+# Start of element C310
+ChangePinName(C310, 1, 1)
+ChangePinName(C310, 2, 2)
+
+# Start of element R302
+ChangePinName(R302, 1, 1)
+ChangePinName(R302, 2, 2)
+
+# Start of element R301
+ChangePinName(R301, 1, 1)
+ChangePinName(R301, 2, 2)
+
+# Start of element C308
+ChangePinName(C308, 1, 1)
+ChangePinName(C308, 2, 2)
+
+# Start of element U301
+ChangePinName(U301, 30, VINR3)
+ChangePinName(U301, 29, VINL3)
+ChangePinName(U301, 28, VINR4)
+ChangePinName(U301, 27, VINL4)
+ChangePinName(U301, 26, MD0)
+ChangePinName(U301, 25, AD)
+ChangePinName(U301, 24, SCL)
+ChangePinName(U301, 23, SDA)
+ChangePinName(U301, 22, GPIO0)
+ChangePinName(U301, 21, GP1/INTA/DMIN)
+ChangePinName(U301, 20, GP2/INTB/DMCLK)
+ChangePinName(U301, 19, GP3/INTC)
+ChangePinName(U301, 18, DOUT)
+ChangePinName(U301, 17, BCK)
+ChangePinName(U301, 16, LRCK)
+ChangePinName(U301, 15, "SCKI (3V3)")
+ChangePinName(U301, 14, IOVDD)
+ChangePinName(U301, 13, DVDD)
+ChangePinName(U301, 12, DGND)
+ChangePinName(U301, 11, LDO)
+ChangePinName(U301, 10, XI)
+ChangePinName(U301, 9, XO)
+ChangePinName(U301, 8, AVDD)
+ChangePinName(U301, 7, AGND)
+ChangePinName(U301, 6, VREF)
+ChangePinName(U301, 5, Mic Bias)
+ChangePinName(U301, 4, VINR1)
+ChangePinName(U301, 3, VINL1)
+ChangePinName(U301, 2, VINR2)
+ChangePinName(U301, 1, VINL2)
+
+# Start of element J101
+ChangePinName(J101, 3, 3)
+ChangePinName(J101, 2, 2)
+ChangePinName(J101, 1, 1)
+
+# Start of element R111
+ChangePinName(R111, 1, 1)
+ChangePinName(R111, 2, 2)
+
+# Start of element R110
+ChangePinName(R110, 1, 1)
+ChangePinName(R110, 2, 2)
+
+# Start of element Q101
+ChangePinName(Q101, S, S)
+ChangePinName(Q101, D, D)
+ChangePinName(Q101, G, G)
+
+# Start of element U101
+ChangePinName(U101, 5, INH)
+ChangePinName(U101, 4, NC)
+ChangePinName(U101, 3, GND)
+ChangePinName(U101, 2, GND)
+ChangePinName(U101, 6, GND)
+ChangePinName(U101, 7, GND)
+ChangePinName(U101, 1, OUT)
+ChangePinName(U101, 8, IN)
+
+# Start of element C102
+ChangePinName(C102, 1, 1)
+ChangePinName(C102, 2, 2)
+
+# Start of element C101
+ChangePinName(C101, 1, 1)
+ChangePinName(C101, 2, 2)
+
+# Start of element J1
+ChangePinName(J1, 31, 31)
+ChangePinName(J1, 32, 32)
+ChangePinName(J1, 33, 33)
+ChangePinName(J1, 34, 34)
+ChangePinName(J1, 35, 35)
+ChangePinName(J1, 36, 36)
+ChangePinName(J1, 39, 39)
+ChangePinName(J1, 37, 37)
+ChangePinName(J1, 40, 40)
+ChangePinName(J1, 38, 38)
+ChangePinName(J1, 26, 26)
+ChangePinName(J1, 27, 27)
+ChangePinName(J1, 28, 28)
+ChangePinName(J1, 29, 29)
+ChangePinName(J1, 30, 30)
+ChangePinName(J1, 25, 25)
+ChangePinName(J1, 21, 21)
+ChangePinName(J1, 24, 24)
+ChangePinName(J1, 23, 23)
+ChangePinName(J1, 22, 22)
+ChangePinName(J1, 11, 11)
+ChangePinName(J1, 12, 12)
+ChangePinName(J1, 13, 13)
+ChangePinName(J1, 14, 14)
+ChangePinName(J1, 15, 15)
+ChangePinName(J1, 16, 16)
+ChangePinName(J1, 19, 19)
+ChangePinName(J1, 17, 17)
+ChangePinName(J1, 20, 20)
+ChangePinName(J1, 18, 18)
+ChangePinName(J1, 6, 6)
+ChangePinName(J1, 7, 7)
+ChangePinName(J1, 8, 8)
+ChangePinName(J1, 9, 9)
+ChangePinName(J1, 10, 10)
+ChangePinName(J1, 5, 5)
+ChangePinName(J1, 1, 1)
+ChangePinName(J1, 4, 4)
+ChangePinName(J1, 3, 3)
+ChangePinName(J1, 2, 2)
+
+# Start of element J3
+ChangePinName(J3, 60, 60)
+ChangePinName(J3, 58, 58)
+ChangePinName(J3, 56, 56)
+ChangePinName(J3, 54, 54)
+ChangePinName(J3, 52, 52)
+ChangePinName(J3, 59, 59)
+ChangePinName(J3, 57, 57)
+ChangePinName(J3, 55, 55)
+ChangePinName(J3, 53, 53)
+ChangePinName(J3, 51, 51)
+ChangePinName(J3, 50, 50)
+ChangePinName(J3, 49, 49)
+ChangePinName(J3, 48, 48)
+ChangePinName(J3, 47, 47)
+ChangePinName(J3, 46, 46)
+ChangePinName(J3, 45, 45)
+ChangePinName(J3, 44, 44)
+ChangePinName(J3, 43, 43)
+ChangePinName(J3, 42, 42)
+ChangePinName(J3, 41, 41)
+ChangePinName(J3, 40, 40)
+ChangePinName(J3, 39, 39)
+ChangePinName(J3, 38, 38)
+ChangePinName(J3, 37, 37)
+ChangePinName(J3, 36, 36)
+ChangePinName(J3, 35, 35)
+ChangePinName(J3, 34, 34)
+ChangePinName(J3, 33, 33)
+ChangePinName(J3, 32, 32)
+ChangePinName(J3, 31, 31)
+ChangePinName(J3, 30, 30)
+ChangePinName(J3, 29, 29)
+ChangePinName(J3, 28, 28)
+ChangePinName(J3, 27, 27)
+ChangePinName(J3, 26, 26)
+ChangePinName(J3, 25, 25)
+ChangePinName(J3, 24, 24)
+ChangePinName(J3, 23, 23)
+ChangePinName(J3, 22, 22)
+ChangePinName(J3, 21, 21)
+ChangePinName(J3, 20, 20)
+ChangePinName(J3, 19, 19)
+ChangePinName(J3, 18, 18)
+ChangePinName(J3, 17, 17)
+ChangePinName(J3, 16, 16)
+ChangePinName(J3, 15, 15)
+ChangePinName(J3, 14, 14)
+ChangePinName(J3, 13, 13)
+ChangePinName(J3, 12, 12)
+ChangePinName(J3, 11, 11)
+ChangePinName(J3, 10, 10)
+ChangePinName(J3, 9, 9)
+ChangePinName(J3, 8, 8)
+ChangePinName(J3, 7, 7)
+ChangePinName(J3, 6, 6)
+ChangePinName(J3, 5, 5)
+ChangePinName(J3, 4, 4)
+ChangePinName(J3, 3, 3)
+ChangePinName(J3, 2, 2)
+ChangePinName(J3, 1, 1)
+
+# Start of element J2
+ChangePinName(J2, 60, 60)
+ChangePinName(J2, 58, 58)
+ChangePinName(J2, 56, 56)
+ChangePinName(J2, 54, 54)
+ChangePinName(J2, 52, 52)
+ChangePinName(J2, 59, 59)
+ChangePinName(J2, 57, 57)
+ChangePinName(J2, 55, 55)
+ChangePinName(J2, 53, 53)
+ChangePinName(J2, 51, 51)
+ChangePinName(J2, 50, 50)
+ChangePinName(J2, 49, 49)
+ChangePinName(J2, 48, 48)
+ChangePinName(J2, 47, 47)
+ChangePinName(J2, 46, 46)
+ChangePinName(J2, 45, 45)
+ChangePinName(J2, 44, 44)
+ChangePinName(J2, 43, 43)
+ChangePinName(J2, 42, 42)
+ChangePinName(J2, 41, 41)
+ChangePinName(J2, 40, 40)
+ChangePinName(J2, 39, 39)
+ChangePinName(J2, 38, 38)
+ChangePinName(J2, 37, 37)
+ChangePinName(J2, 36, 36)
+ChangePinName(J2, 35, 35)
+ChangePinName(J2, 34, 34)
+ChangePinName(J2, 33, 33)
+ChangePinName(J2, 32, 32)
+ChangePinName(J2, 31, 31)
+ChangePinName(J2, 30, 30)
+ChangePinName(J2, 29, 29)
+ChangePinName(J2, 28, 28)
+ChangePinName(J2, 27, 27)
+ChangePinName(J2, 26, 26)
+ChangePinName(J2, 25, 25)
+ChangePinName(J2, 24, 24)
+ChangePinName(J2, 23, 23)
+ChangePinName(J2, 22, 22)
+ChangePinName(J2, 21, 21)
+ChangePinName(J2, 20, 20)
+ChangePinName(J2, 19, 19)
+ChangePinName(J2, 18, 18)
+ChangePinName(J2, 17, 17)
+ChangePinName(J2, 16, 16)
+ChangePinName(J2, 15, 15)
+ChangePinName(J2, 14, 14)
+ChangePinName(J2, 13, 13)
+ChangePinName(J2, 12, 12)
+ChangePinName(J2, 11, 11)
+ChangePinName(J2, 10, 10)
+ChangePinName(J2, 9, 9)
+ChangePinName(J2, 8, 8)
+ChangePinName(J2, 7, 7)
+ChangePinName(J2, 6, 6)
+ChangePinName(J2, 5, 5)
+ChangePinName(J2, 4, 4)
+ChangePinName(J2, 3, 3)
+ChangePinName(J2, 2, 2)
+ChangePinName(J2, 1, 1)
--- boards/gEDA/i2s-mezzanine/makebom.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-gnetlist -g partslist3 -o 999_m9.bom 000_ls_connector.sch 100_power.sch 300_audio.sch 400_audio_filter_conn.sch
-
-#http://wiki.geda-project.org/geda:faq-gnetlist#bill_of_materials_bom_generation
-
